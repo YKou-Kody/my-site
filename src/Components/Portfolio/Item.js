dc01b4ced72990b1dbd5ed90a4d69741b93379e6
YKou-Kody <83717117+YKou-Kody@users.noreply.github.com>
2022-01-12 23:07:33 -0700

    portfolio wip

--- src/Components/Portfolio/Item.js
+++ src/Components/Portfolio/Item.js
@@ -1,26 +1,31 @@
 import React from 'react'
+import { RGB_ETC1_Format } from 'three'
 
-const Item = () => {
+const Item = ({ img, title, description }) => {
     return (
 
         <div>
-            <div className="flex">
-                <div className="w-48 h-80 bg-black rounded-l-2xl">
-                    test
-                    <img src="" />
+            <div className="flex mt-10 rounded-2xl shadow-2xl mb-10"
+                style={{
+                    boxShadow: "5px 5px 1px 1px rgba(0, 0, 0, .3)",
+                    boxShadow:
+                        "inset 0 -3em 3em rgba(0,0,0,0.1),0 0  0 2px rgb(255,255,255), 0.3em 0.3em 1em rgba(0,0,0,0.3)"
+                }}>
+                <div className="w-48 h-80 rounded-l-2xl overflow-hidden"
+                    style={{ width: "200px" }}>
+                    <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                        className="object-cover h-full" />
                 </div>
-                <div className="w-96 bg-gray-400 rounded-r-2xl">
-                    <h1 className="text-3xl">
-                        testtest
+                <div className="w-96 rounded-r-2xl p-1 border-gray-900 bg-gray-900"
+                    style={{ width: "600px", color: "white" }}>
+                    <h1 className="text-3xl font-bold">
+                        {title}
+                        title goes here
                     </h1>
-                    this is a project for my sawsdfalkjsdaslkdj alksdj alskdj alskdj lk
+                    asdasdasdsawsdfalkjsdaslkdj alksdj alskdj alskdj lk
                 </div>
-
-
-
             </div>
-            test
-        </div>
+        </div >
 
     )
 }
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -4,20 +4,35 @@ import Item
     from './Item'
 const Portfolio = () => {
     return (
-        <div>
+        <div className=" v-screen">
             <div className="flex justify-center">
                 <div className="title">
                     <h1 className="text-7xl">
                         Portfolio
+                        <br></br>
                     </h1>
 
                 </div>
-                <div>
-                    <Item />
 
-                </div>
 
             </div>
+
+            <div className="flex justify-center">
+                <Item />
+            </div>
+
+            <div className="flex justify-center">
+                <Item />
+            </div>
+
+            <div className="flex justify-center">
+                <Item />
+            </div>
+
+            <div className="flex justify-center">
+                <Item />
+            </div>
+
         </div>
     )
 }
