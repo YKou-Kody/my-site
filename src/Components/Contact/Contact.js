3b8e97fe9aad936eb6fc9c1b68ab64e5e91eac7f
YKou-Kody <83717117+YKou-Kody@users.noreply.github.com>
2022-01-24 18:05:09 -0700

    Cleared input fields after a message has been sent to my email

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,12 +5,20 @@ import emailjs from '@emailjs/browser';
 const Contact = () => {
     const [status, setStatus] = useState("");
     const form = useRef();
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
     const sendEmail = (e) => {
         e.preventDefault();
+        if (!email || !message) {
+            setStatus('error');
+            return;
+        }
         emailjs.sendForm("service_z246snx", "template_fqtkwdl", form.current,
             "user_YEyFM4UYWLyr4utnBneR1")
             .then(result => {
+                setEmail("");
+                setMessage("");
                 console.log(result)
                 setStatus('success')
             }).catch(err => {
@@ -32,14 +40,17 @@ const Contact = () => {
                 <p class="text-white text-left text-xs italic">If you have any questions, or would like to collaborate on
                     interesting side projects, please leave a message here or message me on LinkedIn and I'll respond in a timely manner, thanks! </p>
                 <div className=" inline-block v-screen ">
-                    <form ref={form} onSubmit={sendEmail} class=" shadow-xl shadow- shadow-purple-500 rounded-3xl px-8 pt-6 pb-3 mt-5 mb-4" >
+                    <form ref={form} onSubmit={sendEmail} class=" shadow-xl shadow- 
+                    shadow-purple-500 rounded-3xl px-8 pt-6 pb-3 mt-5 mb-4" >
                         <div class="mb-6 justify-left bg">
                             <label class="block text-sm font-bold mb-2 text-left text-white" for="username">
                                 Email/Phone number:
                             </label>
                             <input class="shadow border rounded w-full py-2 px-3 text-gray-700 
                             focus:outline-none focus:outline-purple-400 " id="username"
-                                type="text" name="from_name" placeholder="Email" />
+                                type="text" name="from_name" placeholder="Email" value={email} onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }} />
                         </div>
                         <div class="mb-1">
                             <label class="block text-white text-sm font-bold mb-2 text-left" for="message">
@@ -47,7 +58,10 @@ const Contact = () => {
                             </label>
                             <textarea class="shadow rounded w-full py-2 px-3 
                             text-gray-700 mb-3  focus:outline-purple-400 focus:outline-none" id="message"
-                                placeholder="Leave a message here" name="message"
+                                placeholder="Leave a message here" name="message" value={message}
+                                onChange={(e) => {
+                                    setMessage(e.target.value)
+                                }}
                                 rows="5" ></textarea>
 
                         </div>
@@ -64,7 +78,7 @@ const Contact = () => {
                             {status == 'error' ? (
                                 <div>
                                     <p class="text-center text-red-500 text-s">
-                                        Could not send message, please try again.
+                                        Could not send message, make sure both fields are provided and try again.
                                     </p>
                                 </div>
                             ) : ""}
